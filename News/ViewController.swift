32a6e2afdc3c5ec7d2b9e87d28cbff3f99991787
Imanol <imanolof29@gmail.com>
2024-03-02 11:26:40 +0100

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Imanol Ortiz on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
